df9f334b59a449e687d54b88e0008f6407a1dcfc
Spencer <sta7ey@virginia.edu>
2018-01-29 14:05:14 -0500

    Refractored lo_shu

--- lo_shu.py
+++ lo_shu.py
@@ -17,26 +17,8 @@ print("You entered:")
 for row in square:
     print(str(row[0]) + '\t' + str(row[1]) + '\t' + str(row[2]))
 
-# check rows
-for row in square:
-    if sum(row) != 15:
-        is_square = False
-        print(str(row) + " fails the test!")
-
-# check cols
-for i in range(3):
-    if square[0][i] + square[1][i] + square[2][i] != 15:
-        is_square = False
-        print("Column " + str(i) + " fails the test!")
-
-# check diagonals
-if square[0][0] + square[1][1] + square[2][2] != 15:
-    is_square = False
-    print("Left->Right diagonal fails the test!")
-
-if square[0][2] + square[1][1] + square[2][0] != 15:
-    is_square = False
-    print("Right->Left diagonal fails the test!")
+# check square
+is_square = _check('row', square) or _check('cols', square) or _check('diagonals', square)
 
 if not is_square:
     print("This is not a Lo Shu Magic Square!")
@@ -44,4 +26,28 @@ else:
     print("This is a valid Lo Shu Magic Square!")
 
 
+def _check(direction, square):
+    is_square = True
 
+    if direction == "row":
+        for row in square:
+            if sum(row) != 15:
+                is_square = False
+                print(str(row) + " fails the test!")
+
+    elif direction == "cols":
+        for i in range(3):
+            if square[0][i] + square[1][i] + square[2][i] != 15:
+                is_square = False
+                print("Column " + str(i) + " fails the test!")
+
+    elif direction == "diagonals":
+        if square[0][0] + square[1][1] + square[2][2] != 15:
+            is_square = False
+            print("Left->Right diagonal fails the test!")
+
+        if square[0][2] + square[1][1] + square[2][0] != 15:
+            is_square = False
+            print("Right->Left diagonal fails the test!")
+
+    return is_square
