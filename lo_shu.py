340193274117c91e77f5b5775670abf1bdd50576
Piecemaker96 <31420748+Piecemaker96@users.noreply.github.com>
2018-01-29 14:43:25 -0500

    Update lo_shu.py

--- lo_shu.py
+++ lo_shu.py
@@ -21,27 +21,27 @@ for row in square:
 for row in square:
     if sum(row) != 15:
         is_square = False
-        print(str(row) + " fails the test!")
+        print(str(row) + " fails the test! lul faillllllll")
 
 # check cols
 for i in range(3):
     if square[0][i] + square[1][i] + square[2][i] != 15:
         is_square = False
-        print("Column " + str(i) + " fails the test!")
+        print("Column " + str(i) + " fails the test! why did you think that would work ?!?!?!?")
 
 # check diagonals
 if square[0][0] + square[1][1] + square[2][2] != 15:
     is_square = False
-    print("Left->Right diagonal fails the test!")
+    print("Left->Right diagonal fails the test! jeBaited jeBaited jeBaited")
 
 if square[0][2] + square[1][1] + square[2][0] != 15:
     is_square = False
-    print("Right->Left diagonal fails the test!")
+    print("Right->Left diagonal fails the test! Kappa Kappa Kappa")
 
 if not is_square:
-    print("This is not a Lo Shu Magic Square!")
+    print("This is not a Lo Shu Magic Square! what were you thinking lol?")
 else:
-    print("This is a valid Lo Shu Magic Square!")
+    print("This is a valid Lo Shu Magic Square! congrats, genius")
 
 
 
